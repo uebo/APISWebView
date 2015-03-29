bfb97efcd33de6f169291ad1b583ce7e05564393
Yoji Ueda <bluezbz@gmail.com>
2015-03-29 17:20:17 +0900

    first commit

--- APISWebView/AppDelegate.m
+++ APISWebView/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AppiariesSDK/AppiariesSDK.h>
 
 @interface AppDelegate ()
 
@@ -16,30 +17,80 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    //アピアリーズのセッションを初期化する
+    [[APISSession sharedSession] configureWithDatastoreId:@"_sandbox" applicationId:@"APISWebView" applicationToken:@"app41b90619525d1529c93763da62"];
+    
+    // APNs: プッシュ通知機能利用登録（デバイストークン発行要求）
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+#ifdef __IPHONE_8_0
+        // iOS8以降のプッシュ通知登録処理
+        UIUserNotificationType types = UIUserNotificationTypeBadge |  UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+        UIUserNotificationSettings *notifSettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:notifSettings];
+#endif
+    } else {
+        // iOS8以前のプッシュ通知登録処理
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    }
+    //開封通知
+    NSDictionary *notificationUserInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    if(notificationUserInfo){
+        [self showPushNotificationAlertViewIfNeeded:notificationUserInfo];
+    }
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+#ifdef __IPHONE_8_0
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+    [application registerForRemoteNotifications];
+}
+#endif
+
+// APNs: デバイストークン発行成功時ハンドラ
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"APNs: デバイストークン発行成功 [デバイストークン:%@]", [deviceToken description]);
+    // デバイストークン登録APIの実行
+    APISPushAPIClient *api = [[APISSession sharedSession] createPushAPIClient];
+    [api registerDeviceToken:deviceToken attributes:nil];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+// APNs: デバイストークン発行失敗時ハンドラ
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"APNs: デバイストークン発行失敗 [原因:%@]", [error localizedDescription]);
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+//アプリがフォアグラウンド or バックグラウンドでのリモート通知処理
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    //開封通知
+    [self showPushNotificationAlertViewIfNeeded:userInfo];
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+- (void)showPushNotificationAlertViewIfNeeded:(NSDictionary*)userInfo
+{
+    //通知＆バッジを消す
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 0];
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    
+    NSDictionary *aps = [userInfo objectForKey:@"aps"];
+    if (!aps) {
+        return;
+    }
+    //プッシュ通知メッセージを表示
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"AlertTitle", nil)
+                                                    message:aps[@"alert"]
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+    
+    //APIS PUSH 開封通知を送信（userInfoのpushIdキーにトークンが入っている）
+    NSString* pushId = (NSString*) [userInfo objectForKey:@"pushId"];
+    if (pushId != nil) {
+        APISPushAPIClient *api = [[APISSession sharedSession] createPushAPIClient];
+        [api notifyMessageOpenedWithPushId:[pushId integerValue]];
+    }
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
--- APISWebView/ViewController.m
+++ APISWebView/ViewController.m
@@ -8,7 +8,19 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *const kStartUrlStringUserDefaults = @"kStartUrlStringUserDefaults";
+static NSString *const kDefaultStartUrl = @"http://www.appiaries.com/";
+
+@interface ViewController () <UIWebViewDelegate, UIAlertViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButtonItem;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardButtonItem;
+- (IBAction)backButtonAction:(id)sender;
+- (IBAction)forwardButtonAction:(id)sender;
+- (IBAction)refreshButtonAction:(id)sender;
+- (IBAction)settingButtonAction:(id)sender;
+
 
 @end
 
@@ -16,12 +28,105 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self loadWebView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIWebViewDelegate
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    self.backButtonItem.enabled = NO;
+    self.forwardButtonItem.enabled = NO;
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    [self finishedLoadWebView:webView];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self finishedLoadWebView:webView];
+}
+
+-(void)finishedLoadWebView:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    if (webView.canGoBack) {
+        self.backButtonItem.enabled = YES;
+    } else {
+        self.backButtonItem.enabled = NO;
+    }
+    if (webView.canGoForward) {
+        self.forwardButtonItem.enabled = YES;
+    } else {
+        self.forwardButtonItem.enabled = NO;
+    }
+}
+
+#pragma mark - Actions
+- (IBAction)backButtonAction:(id)sender {
+    [self.webView goBack];
+}
+
+- (IBAction)forwardButtonAction:(id)sender {
+    [self.webView goForward];
+}
+
+- (IBAction)refreshButtonAction:(id)sender {
+    [self.webView reload];
+}
+
+- (IBAction)settingButtonAction:(id)sender {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
+                                                    message:@"Starting URL setting"
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancel"
+                                          otherButtonTitles:@"OK", nil];
+    [alert setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    UITextField *textField = [alert textFieldAtIndex:0];
+    NSUserDefaults* ud = [NSUserDefaults standardUserDefaults];
+    textField.text = [ud objectForKey:kStartUrlStringUserDefaults];
+    [alert show];
+}
+
+#pragma mark - UIAlertViewDelegate
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+        //表示するURLの保存処理
+        NSString *urlString = [[alertView textFieldAtIndex:0]text] ? :@"";
+        [self saveUrl:urlString];
+        //URLをロードする
+        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    }
+}
+
+#pragma mark - Private method
+-(void)loadWebView
+{
+    //表示するURL
+    NSUserDefaults* ud = [NSUserDefaults standardUserDefaults];
+    NSString *urlString = [ud objectForKey:kStartUrlStringUserDefaults];
+    if (urlString == nil) {
+        //初期値はアピアリーズのHP
+        urlString = kDefaultStartUrl;
+        [self saveUrl:urlString];
+    }
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+}
+
+-(void)saveUrl:(NSString*)urlString
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:urlString forKey:kStartUrlStringUserDefaults];
+    [userDefaults synchronize];
 }
 
 @end
